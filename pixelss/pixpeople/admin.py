226301210c62bd85fb3d87c7c469a473d2f7ad5c
new name <zhukov.msp@gmail.com>
2021-08-19 23:46:36 +0300

    Некоторые изменения в html страницах + небольшая настройка админ панели.

--- pixelss/pixpeople/admin.py
+++ pixelss/pixpeople/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'time_create', 'is_published')
+
+
+admin.site.register(People, PeopleAdmin)
+
--- pixelss/pixpeople/models.py
+++ pixelss/pixpeople/models.py
@@ -3,5 +3,16 @@ from django.db import models
 # Create your models here.
 
 
-def __str__(self):
-    return self.title
+class People(models.Model):
+    content = models.TextField(blank=True, verbose_name="Сообщение")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время отправки")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Последние изменение")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name = 'Чат'
+        verbose_name_plural = 'Чат'
+        ordering = ['time_create', 'content']
